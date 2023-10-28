6b7eb7d5e005d57e887acd28b011a6bfdd31f30d
afadza <arya.skoba@gmail.com>
2023-10-28 15:37:38 +0700

    auth

--- src/middlewares/UploadFile.ts
+++ src/middlewares/UploadFile.ts
@@ -2,10 +2,10 @@ import * as multer from "multer";
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, "src/uploads"); // Menyimpan file di direktori './public/image'
+		cb(null, "src/uploads"); 
 	},
 	filename: (req, file, cb) => {
-		cb(null, `${Date.now()}-${file.originalname}`); // Menetapkan nama file
+		cb(null, `${Date.now()}-${file.originalname}`);
 	},
 });
 
